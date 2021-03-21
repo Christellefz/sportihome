df964ae338dd2a177a9597ba1b4f0ab218f6a5ed
Christellefz <christellefz@yahoo.fr>
2021-03-21 11:09:51 +0100

    mes fakes données s'affichent sur la carte

--- test-front/src/App.js
+++ test-front/src/App.js
@@ -2,13 +2,46 @@ import React, {Component} from "react";
 import {MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import L from 'leaflet';
 import Form from './components/Form';
-//import jaune from './assets/jaune.svg';
+//import * as fakeData from './components/Data'
+import CYCLING from './assets/CYCLING.svg';
 import rouge from './assets/rouge.svg';
-//import noir from './assets/noir.svg';
+import KITEBOARDING from './assets/KITEBOARDING.svg';
+import YOGA from './assets/YOGA.svg';
 import "./App.css";
 
 
-
+const fakeData= [
+  {
+      id:1,
+      Spot: "Mon super spot1",
+      coordonnées:[
+      43.604510,
+      3.902116
+      ],
+      Sport: "Velo",
+      Favori: true
+  },
+  {
+      id:3,
+      Spot: "Mon super spot2",
+      coordonnées:[
+      43.522663,
+      3.922588
+      ],
+      Sport: "Kite",
+      Favori: false
+  },
+  {
+      id:1,
+      Spot: "Mon super spot3",
+      coordonnées:[
+      43.556386,
+      3.927683
+      ],
+      Sport: "Yoga",
+      Favori: true
+  }
+  ]
  
 
 class App extends Component  {
@@ -17,14 +50,33 @@ class App extends Component  {
         lng: 3.876716,
         zoom: 13
   }
-
+  
+//List of icons
   grenIcon = L.icon({
     iconUrl: rouge,
     iconSize:     [38, 95], // size of the icon
     iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
     popupAnchor:  [-3, -76]
   });
+  VeloIcon = L.icon({
+    iconUrl: CYCLING,
+    iconSize:     [38, 95], // size of the icon
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    popupAnchor:  [-3, -76]
+  });
+  KiteIcon = L.icon({
+    iconUrl: KITEBOARDING,
+    iconSize:     [38, 95], // size of the icon
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    popupAnchor:  [-3, -76]
+  });
 
+  YogaIcon = L.icon({
+    iconUrl: YOGA,
+    iconSize:     [38, 95], // size of the icon
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    popupAnchor:  [-3, -76]
+  });
   //Update position to user's position
   getLocation=()=> {
     navigator.geolocation.getCurrentPosition((position, error) =>{
@@ -52,7 +104,8 @@ render(){
   
     const positionGreenIcon = [this.state.lat, this.state.lng];
     console.log(this.state.lat, this.state.lng)
-    
+    console.log(fakeData)
+
   return (
     <div className= "structure">
     <div className="map">
@@ -66,6 +119,19 @@ render(){
           You are here
           </Popup>
         </Marker>
+      {fakeData.map(fake =>(
+          <Marker key={fake.id} 
+          position ={fake.coordonnées} 
+          icon={ (fake.Sport=== 'Velo')
+          ? this.VeloIcon
+          : (fake.Sport=== 'Kite')
+          ? this.KiteIcon
+          : this.YogaIcon}> 
+          <Popup>
+              {fake.Spot}
+          </Popup>
+          </Marker>
+       ))}
       </MapContainer>
       <Form/>
       </div>
--- test-front/src/components/Data.js
+++ test-front/src/components/Data.js
@@ -6,21 +6,22 @@ const fakeData= [
     Longitude:3.902116,
     Sport: "Velo",
     Favori: true
-}
+},
 {
-    id:2,
+    id:3,
     Spot: "Mon super spot2",
-    Latitude: 43.604510,
-    Longitude:3.902116,
+    Latitude: 43.522663,
+    Longitude:3.922588,
     Sport: "Kite",
     Favori: false
-}
+},
 {
     id:1,
-    Spot: "Mon super spot1",
-    Latitude: 43.604510,
-    Longitude:3.902116,
+    Spot: "Mon super spot3",
+    Latitude: 43.556386,
+    Longitude:3.927683,
     Sport: "Yoga",
     Favori: true
 }
 ]
+export default fakeData
