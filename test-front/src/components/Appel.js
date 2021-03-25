6ee410a3e3abd249b5bfbb2267141d428f257d6c
Christellefz <christellefz@yahoo.fr>
2021-03-25 10:24:08 +0100

    réécriture d'appel avec queryparameter

--- test-front/src/components/Appel.js
+++ test-front/src/components/Appel.js
@@ -2,12 +2,15 @@ import React, {useState} from 'react';
 import axios from 'axios';
 
 const Appel= () =>{
-const [poulet, setPoulet]=useState([]) 
-    axios.get("https://miadil.github.io/starwars-api/api/all.json")
+const [poulet, setPoulet]=useState([]) ;
+let iciLesCoordNE =[44.00, 4.00]
+let iciLesCoordSW=[45.00, 5.00]
+
+    axios.get(`Localhost:3000/api/spots/getAllMarkersInBounds/?northeast=${iciLesCoordNE}&southweast=${iciLesCoordSW}`,)
     .then((res) =>setPoulet(res.data));
     console.log(poulet[0])
     return(
-        <div>test</div>
+        <div>test:{poulet}</div>
     )
 }
 export default Appel
